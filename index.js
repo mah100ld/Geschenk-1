1c225162cf4dd7af2fb3bf6a8245dc639032abdf
Maximilian Horst <mahorst@deloitte.de>
2021-08-28 13:13:57 +0200

    Check what happens without build

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-// const express = require('express')
-// const path = require('path')
-const PORT = process.env.PORT || 5000
-// const app = express()
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('index.html'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-// const path = require('path')
-// express()
-// .use('/', express.static(path.join(__dirname, 'src')))
-
-// express
-
-// app.get('/', function (req, res) {
-//     res.render('views/index');
-//   });
-
-// app.listen(PORT, () => {
-//     console.log(`Example app listening at http://localhost:${PORT}`)
-//   })
-  
   const express = require('express');
   const path = require('path');
+  const PORT = process.env.PORT || 5000
   const app = express();
   
   app.use(express.static(path.join(__dirname, 'views')));
