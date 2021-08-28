094735e214327305ed805141d3141725588689da
Maximilian Horst <mahorst@deloitte.de>
2021-08-28 12:54:09 +0200

    App structure finished

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {padding: { "fluid-video": "56.25%" }},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
