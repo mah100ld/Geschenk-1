1d6737a0262689b523077bbef89f6136827f0a76
Maximilian Horst <mahorst@deloitte.de>
2021-08-28 13:05:41 +0200

    Port updadet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const express = require('express')
 // const path = require('path')
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 // const app = express()
 
 // express()
@@ -34,4 +34,4 @@
     res.render(path.join(__dirname, 'views', 'index.html'));
   });
   
-  app.listen(5000);
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
